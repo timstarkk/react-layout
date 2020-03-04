62897767e3157505f25b1e8962ac36af2bf7bf3b
timstarkk <timstarkk@icloud.com>
2020-03-04 01:35:32 -0600

    created pages folder with Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero';
+// import Banner from '../components/Banner';
+// import { Link } from 'react-router-dom';
+
+
+export default function Home() {
+    return (
+        <>
+            <Hero>
+                {/* <Banner /> */}
+            </Hero>
+
+        </>
+    )
+}
