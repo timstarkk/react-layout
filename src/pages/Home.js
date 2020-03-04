04a03523c82a80c7780c449ae0a0f3e49edb7d97
timstarkk <timstarkk@icloud.com>
2020-03-04 01:55:07 -0600

    Info import and added under Hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero/Hero';
 // import Banner from '../components/Banner';
 // import { Link } from 'react-router-dom';
+import Info from '../components/Info/Info';
 
 
 export default function Home() {
@@ -10,7 +11,7 @@ export default function Home() {
             <Hero>
                 {/* <Banner /> */}
             </Hero>
-
+            <Info />
         </>
     )
 }
