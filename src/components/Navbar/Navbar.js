e2675845526574c387367c28899a21817c1530a2
timstarkk <timstarkk@icloud.com>
2020-03-07 17:03:20 -0600

    added navbar scroll styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,29 +6,43 @@ import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     state = {
-        isOpen: false
+        isOpen: false,
+        scrolled: false
     }
+
+    listenScrollEvent = e => {
+        if (window.scrollY > 400) {
+            this.setState({ scrolled: true })
+        } else {
+            this.setState({ scrolled: false })
+        }
+    }
+
 
     handleToggle = () => {
         this.setState({ isOpen: !this.state.isOpen })
     }
 
+    componentDidMount() {
+        window.addEventListener("scroll", this.listenScrollEvent)
+    }
+
     render() {
         return (
-            <nav className="navbar">
+            <nav className={"navbar " + (this.state.scrolled ? "nav-scrolled" : "")}>
                 <div className="nav-center">
                     <div className="nav-header">
                         <Link to="/" className="text-link">
                             <div className="logo-div">
-                                <p className="logo-text">Tim Stark</p>
+                                <p className={"logo-text " + (this.state.scrolled ? "nav-scrolled" : "")}>Tim Stark</p>
                             </div>
                             {/* <img src={logo} alt="logo" style={{ width: "250px", height: "auto" }} /> */}
                         </Link>
                         <button type="button" className="nav-btn" onClick={this.handleToggle}>
-                            <Hamburger className="nav-icon" />
+                            <Hamburger className={"nav-icon " + (this.state.scrolled ? 'nav-scrolled' : '')} />
                         </button>
                     </div>
-                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+                    <ul className={this.state.isOpen ? "nav-links show-nav " + (this.state.scrolled ? 'nav-scrolled' : '') : "nav-links " + (this.state.scrolled ? 'nav-scrolled' : '')}>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
