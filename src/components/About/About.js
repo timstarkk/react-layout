28f3e0f53f2767a99af12fdff9093dda6ec88976
timstarkk <timstarkk@icloud.com>
2020-03-07 16:18:49 -0600

    added about section layout

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+    return (
+        <div className="about-section">
+            <div className="container">
+                <h1>Doing what I love.</h1>
+                <p>I am a Full-Stack Developer who is design-minded. Most of my work has been with JavaScript. Although I am currently in love with the M.E.R.N. stack, I always enjoy learning and working with new technologies. You can view my <a href="https://drive.google.com/file/d/1ryocksh7vhpRQTMPjuH5V1NDCsAr2ep6/view?usp=sharing"
+                    target="_blank">resume here</a>, and you can view some of my latest projects down below.</p>
+            </div>
+        </div>
+    )
+}
