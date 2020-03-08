45262ef45a3a2b1f49a9825245d05cd616dcb6d6
timstarkk <timstarkk@icloud.com>
2020-03-07 19:32:32 -0600

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="container">
+                <p>
+                    Tim Stark 2020
+                </p>
+            </div>
+        </div>
+    )
+}
