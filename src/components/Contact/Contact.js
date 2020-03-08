9ede939f4fd3cf875a9c228341833dcc093f5c2c
timstarkk <timstarkk@icloud.com>
2020-03-07 19:18:26 -0600

    added contact section layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import './Contact.css';
+import { FaMapMarkedAlt, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { MdEmail, MdPhoneIphone } from 'react-icons/md';
 
 export default function Contact() {
     return (
-        <div>
-            hello from contact.
+        <div className="contact-section">
+            <div className="container contact-container">
+                <section className="contact-card">
+                    <div className="contact-card-body">
+                        <FaMapMarkedAlt className="contact-icon" />
+                        <h4>LOCATION</h4>
+                        <div className="card-line" />
+                        <p>Austin, TX</p>
+                    </div>
+                </section>
+
+                <section className="contact-card">
+                    <div className="contact-card-body">
+                        <MdEmail className="contact-icon" />
+                        <h4>EMAIL</h4>
+                        <div className="card-line" />
+                        <p>tim@timstark.dev</p>
+                    </div>
+                </section>
+
+                <section className="contact-card">
+                    <div className="contact-card-body">
+                        <MdPhoneIphone className="contact-icon" />
+                        <h4>PHONE</h4>
+                        <div className="card-line" />
+                        <p>+1 (281) 732-1836</p>
+                    </div>
+                </section>
+            </div>
+            <div className="social">
+                <a href="https://www.linkedin.com/in/tim-stark-00722a113/" className="icon-button" target="_blank">
+                    <FaLinkedin />
+                </a>
+                <a href="https://www.github.com/timstarkk/" className="icon-button" target="_blank">
+                    <FaGithub />
+                </a>
+            </div>
         </div>
     )
 }
