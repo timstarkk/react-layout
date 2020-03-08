d0e8b8852022ddc390a7fa3586567ef4e5e919e9
timstarkk <timstarkk@icloud.com>
2020-03-07 19:32:41 -0600

    added footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Banner from '../components/Banner/Banner';
 import About from '../components/About/About';
 import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
 
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
             <About />
             <Projects />
             <Contact />
+            <Footer />
         </>
     )
 }
