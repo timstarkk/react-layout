20269ffb77b5d2443893dd8115310066aa9228f6
timstarkk <timstarkk@icloud.com>
2020-03-08 19:12:26 -0500

    added id=about for link functionality

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.css';
 
 export default function About() {
     return (
-        <div className="about-section">
+        <div className="about-section" id="about">
             <div className="container">
                 <h1>Doing what I love.</h1>
                 <p>I am a Full-Stack Developer who is design-minded. Most of my work has been with JavaScript. Although I am currently in love with the M.E.R.N. stack, I always enjoy learning and working with new technologies. You can view my <a href="https://drive.google.com/file/d/1ryocksh7vhpRQTMPjuH5V1NDCsAr2ep6/view?usp=sharing"
