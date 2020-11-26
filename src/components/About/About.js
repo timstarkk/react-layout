764f4e13005b2029fe6bcece2609d46d79409c65
timstarkk <tim@timstark.dev>
2020-11-26 16:35:22 -0600

    rewording.... begun to began

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className="about-section" id="about">
             <div className="container">
                 <h1>About Me</h1>
-                <p>I recently earned a certificate in Full-Stack Web Development from The University of Texas at Austin. Since graduating I have continued to grow my JavaScript and React.js skills, as well as gaining hands on experience with AWS cloud technologies. Through online courses, I have also gained a strong understanding of development fundamentals, such as data structures and algorithms. I am currently working on a real-world React.js B2B application, as well as creating my first React Native app using Expo. I have also begun to contribute to open source projects. You can view my <a href="https://docs.google.com/document/d/16CBymhIZhwvpoZPUm7yuFfQjy9NLP9za-iDb6BYyBYk/edit?usp=sharing"
+                <p>I recently earned a certificate in Full-Stack Web Development from The University of Texas at Austin. Since graduating I have continued to grow my JavaScript and React.js skills, as well as gaining hands on experience with AWS cloud technologies. Through online courses, I have also gained a strong understanding of development fundamentals, such as data structures and algorithms. I am currently working on a real-world React.js B2B application, as well as creating my first React Native app using Expo. I have also began contributing to open source projects. You can view my <a href="https://docs.google.com/document/d/16CBymhIZhwvpoZPUm7yuFfQjy9NLP9za-iDb6BYyBYk/edit?usp=sharing"
                     target="_blank">resume here</a>, and my latest projects down below.</p>
             </div>
         </div>
